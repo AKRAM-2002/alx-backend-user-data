94916557601251f8fb8fe7fd5949238fcd54854b
AKRAM-2002 <Akrambusiness6@gmail.com>
2024-11-12 21:00:58 +0100

    update

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+'''
+class Auth to the API authentication
+'''
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Determines if a path requires authentication
+        Currently returns False for all paths.
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Retrieves the Authorization header from the request
+        Currently returns None as a placeholder.
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Placeholder for retrieving the current user
+        Currently returns None.
+        """
+        return None
