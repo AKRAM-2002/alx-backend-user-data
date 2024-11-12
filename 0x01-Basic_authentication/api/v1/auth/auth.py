2970518c92fcf220bcc3d76fd40432c1aea0169f
AKRAM-2002 <Akrambusiness6@gmail.com>
2024-11-12 21:06:26 +0100

    update

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,6 +7,8 @@ from typing import List, TypeVar
 
 
 class Auth:
+    """Authentication class.
+    """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
         Determines if a path requires authentication
