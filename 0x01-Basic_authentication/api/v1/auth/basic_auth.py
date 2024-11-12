e71eece654a6f5787a3986faca16e6f4007a06ae
AKRAM-2002 <Akrambusiness6@gmail.com>
2024-11-12 23:13:18 +0100

    update

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,14 +2,18 @@
 '''
 Basic authentication module for the API.
 '''
-from .auth import Auth
 
+import re
+import base64
+import binascii
+
+from .auth import Auth
 
 class BasicAuth(Auth):
     """class BasicAuth
     """
     def extract_base64_authorization_header(
-    self, authorization_header: str) -> str:
+        self, authorization_header: str) -> str:
         """Extracts the Base64 part of the Authorization header
         for a Basic Authentication.
         """  
@@ -19,3 +23,19 @@ class BasicAuth(Auth):
             if field_match is not None:
                 return field_match.group('token')
         return None
+
+    def decode_base64_authorization_header(
+                self,
+                base64_authorization_header: str,
+                ) -> str:
+        """Decodes a base64-encoded authorization header.
+        """
+        if type(base64_authorization_header) == str:
+            try:
+                res = base64.b64decode(
+                    base64_authorization_header,
+                    validate=True,
+                )
+                return res.decode('utf-8')
+            except (binascii.Error, UnicodeDecodeError):
+                return None
