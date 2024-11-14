e9e4fe4feb688a502258fafa62cd6e1334b9ca04
AKRAM-2002 <Akrambusiness6@gmail.com>
2024-11-14 22:53:18 +0100

    update

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -59,3 +59,27 @@ class BasicAuth(Auth):
                     password = field_match.group('password')
                     return user, password
             return None, None
+
+    def user_object_from_credentials(self, 
+                user_email: str, user_pwd: str
+                ) -> TypeVar('User'):
+            """Extract User based on his email and password
+            """
+            try:
+                users = User.search({'email': user_email})
+            except Exception:
+                return None
+            if len(users) <= 0:
+                return None
+            if users[0].is_valid_password(user_pwd):
+                return users[0]
+            
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Retrieves the user from a request.
+        """
+        auth_header = self.authorization_header(request)
+        b64_auth_token = self.extract_base64_authorization_header(auth_header)
+        auth_token = self.decode_base64_authorization_header(b64_auth_token)
+        email, password = self.extract_user_credentials(auth_token)
+        return self.user_object_from_credentials(email, password)
