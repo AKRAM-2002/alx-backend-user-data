2409247fd11e02b1048c624ce0d15cc7b1ad0dbc
AKRAM-2002 <Akrambusiness6@gmail.com>
2024-11-17 08:07:50 +0100

    update

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -8,9 +8,21 @@ import binascii
 from typing import Tuple, TypeVar
 from .auth import Auth
 from models.user import User
+import uuid
 
 
 class SessionAuth(Auth):
     """class BasicAuth
     """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        '''Creates a Session ID for a user_id
+        '''
+        if type(user_id) == str:
+            id_session = str(uuid.uuid4())
+            self.user_id_by_session_id[id_session] = user_id
+            return id_session
+
+        return None
     
